274dccd75c5428d294bdd0d0dde14b236d3e57b5
Jason McDonald <jasonmcd65@gmail.com>
2021-01-22 20:50:47 -0600

    wrote burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,24 @@
- 
+//  Imports the orm and uses that to interact with the database
+// This file gets imported into the the controller
+
+let orm = require('../config/orm')
+
+let burger = {
+    all: function(callback){
+        orm.selectAll('burgers', function(res){
+            callback(res)
+        });
+    },
+    insert: function(columns, values, callback){
+        orm.insertOne('burgers', columns, values, function(res){
+            callback(res);
+        });
+    },
+    update: function(objColValues, condition, callback) {
+        orm.updateOne('burgers', objColValues, condition, function(res){
+            callback(res);
+        });
+    }
+}
+
+module.exports = burger
