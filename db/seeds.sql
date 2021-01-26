fd89ebdb5cbcd5a2f2131614971175169395cc9f
Jason McDonald <jasonmcd65@gmail.com>
2021-01-25 20:35:32 -0600

    nothing was changed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
-insert into burgers (burger_name, devoured) values ('Mushroom Swiss', false), ('Monster Burger', false), ('Impossible Burger', false)
+insert into burgers (burger_name, devoured) values ('Mushroom Swiss', false), ('Monster Burger', false), ('Impossible Burger', false);
+
+insert into burgers (burger_name, devoured) values ('Yummy Burger', 0);
+insert into burgers (burger_name, devoured) values ('Other Burger', 1);
